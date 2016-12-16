7b4c1eab5b91e94a3257236e2503869a763a2131
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-12-16 14:30:49 +0100

    cosmetic: don't log fsm instance addresses
    Change-Id: I8ffbeecdddbef4db4e23722c8300bced7d06caec

--- openbsc/src/osmo-nitb/bsc_hack.c
+++ openbsc/src/osmo-nitb/bsc_hack.c
@@ -355,6 +355,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
+	osmo_fsm_log_addr(false);
 	msc_vlr_init(tall_bsc_ctx, bsc_gsmnet->gsup_server_addr_str,
 		     bsc_gsmnet->gsup_server_port);
 

e2be8c4cfe99d88cb25b797c51145b117676c778
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-12-19 01:33:47 +0100

    vsub
    Change-Id: I2432fd147a2ae23341b58a6e744e397450057538

--- openbsc/src/libcommon/gsm_subscriber_base.c
+++ openbsc/src/libcommon/gsm_subscriber_base.c
@@ -103,7 +103,8 @@ struct gsm_subscriber *subscr_put(struct gsm_subscriber *subscr)
 {
 	subscr->use_count--;
 	DEBUGP(DREF, "subscr %s usage decreases to: %d\n",
-			subscr->extension, subscr->use_count);
+	       subscr->vsub? subscr->vsub->msisdn : subscr->extension,
+	       subscr->use_count);
 	if (subscr->use_count <= 0 &&
 	    !((subscr->group && subscr->group->keep_subscr) ||
 	      subscr->keep_in_ram))
